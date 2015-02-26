f5d43fe32f6915b85db0e9fd96f73640bba5e6f9
josdem <joseluis.delacruz@gmail.com>
2015-02-26 14:52:57 -0600

    first commit

---
+++ src/main/java/com/josdem/bean/EmployeeType.java
@@ -0,0 +1,5 @@
+package com.josdem.bean;
+
+public enum EmployeeType {
+	ENGINEER, SALESMAN, MANAGER
+}
---
+++ src/main/java/com/josdem/refactoring/Employee.java
@@ -0,0 +1,27 @@
+package com.josdem.refactoring;
+
+import java.math.BigDecimal;
+
+import com.josdem.bean.EmployeeType;
+
+public class Employee {
+
+	private final BigDecimal monltySalary = new BigDecimal(100.00);
+	private final BigDecimal bonus = new BigDecimal(20.00);
+	private final BigDecimal commission = new BigDecimal(10.00);
+
+	public BigDecimal payAmount(EmployeeType type) {
+		
+		switch(type){
+			case ENGINEER:
+				return monltySalary;
+			case SALESMAN:
+				return monltySalary.add(commission);
+			case MANAGER:
+				return monltySalary.add(bonus);	
+			default:
+				throw new RuntimeException("Incorrect Employee");
+		}
+	}
+
+}
