6143635674aecc8f10a9f2cbeccb69b6b4541eda
josdem <joseluis.delacruz@gmail.com>
2015-03-04 16:26:20 -0600

    ParkingLot example complete

--- src/test/java/com/josdem/refactoring/TestParkingLot.java
+++ src/test/java/com/josdem/refactoring/TestParkingLot.java
@@ -1,5 +1,46 @@
 package com.josdem.refactoring;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.josdem.refactoring.exception.ParkingLotOverFlowException;
+
 public class TestParkingLot {
+	
+	private static final int MAX_CAPACITY = 5;
+	private ParkingLot parkingLot = new ParkingLot();
+	
+	@Test
+	public void shouldKnowIfParkingLotIsEmpty() throws Exception {
+		assertTrue(parkingLot.isEmpty());
+	}
+	
+	@Test
+	public void shouldPark() throws Exception {
+		parkingLot.park();
+		assertFalse(parkingLot.isEmpty());
+	}
+	
+	@Test
+	public void shouldFullAParkingLot() throws Exception {
+		fullMyParkingLot();
+	}
+
+	@Test(expected=ParkingLotOverFlowException.class)
+	public void shouldNotParkSinceIsFull() throws Exception {
+		fullMyParkingLot();
+		parkingLot.park();
+	}
+	
+	private void fullMyParkingLot() throws ParkingLotOverFlowException {
+		assertFalse(parkingLot.isFull());
+		for(int i=0;i<MAX_CAPACITY;i++){
+			parkingLot.park();
+		}
+		assertTrue(parkingLot.isFull());
+	}
+	
 
 }
