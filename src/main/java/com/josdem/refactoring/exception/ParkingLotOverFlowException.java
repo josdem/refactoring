b24c2bafb6aad0657c103cfa5f653831057b7e95
josdem <joseluis.delacruz@gmail.com>
2015-03-04 16:34:30 -0600

    Duplicated avoiding example completed

---
+++ src/main/java/com/josdem/refactoring/exception/ParkingLotOverFlowException.java
@@ -0,0 +1,10 @@
+package com.josdem.refactoring.exception;
+
+public class ParkingLotOverFlowException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
