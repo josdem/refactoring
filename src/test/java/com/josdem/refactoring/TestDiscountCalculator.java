beb2c1692d235af12fb394d1a4d46243c483c6c2
josdem <joseluis.delacruz@gmail.com>
2015-03-07 15:34:32 -0600

    Adding get price test

--- src/test/java/com/josdem/refactoring/TestDiscountCalculator.java
+++ src/test/java/com/josdem/refactoring/TestDiscountCalculator.java
@@ -5,10 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestDiscountCalculator {
+	
+	private DiscountCalculator discountCalculator = new DiscountCalculator();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void shouldGetMinDiscountFactor() throws Exception {
+		Integer quantity = 10;
+		Integer itemPrice = 5;
+		Double expectedResult = 49.0;
+		
+		Double result = discountCalculator.getPrice(quantity, itemPrice);
+		
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	public void shouldGetMaxDiscountFactor() throws Exception {
+		Integer quantity = 250;
+		Integer itemPrice = 5;
+		Double expectedResult = 1187.5;
+		
+		Double result = discountCalculator.getPrice(quantity, itemPrice);
+		
+		assertEquals(expectedResult, result);
 	}
 
 }
