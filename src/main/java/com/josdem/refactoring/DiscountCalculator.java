b4df4d53b88931ee8bd7bbbb09ad4a01e569dd84
josdem <joseluis.delacruz@gmail.com>
2015-03-07 15:16:44 -0600

    Adding detPrice method

--- src/main/java/com/josdem/refactoring/DiscountCalculator.java
+++ src/main/java/com/josdem/refactoring/DiscountCalculator.java
@@ -1,5 +1,18 @@
 package com.josdem.refactoring;
 
+
 public class DiscountCalculator {
+	
+	Double getPrice(Integer quantity, Integer itemPrice){
+		Integer basePrice = quantity * itemPrice;
+		
+		Double discountFactor;
+		if(basePrice > 1000){
+			discountFactor = 0.95;
+		} else {
+			discountFactor = 0.98;
+		}
+		return basePrice * discountFactor;
+	}
 
 }
