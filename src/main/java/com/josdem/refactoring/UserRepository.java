2be0b13b2644f1de9991ab98b44a58cfcfe8cfcb
josdem <joseluis.delacruz@gmail.com>
2015-03-08 18:41:57 -0600

    Adding Mockito and setup avoid long parameter list with object

---
+++ src/main/java/com/josdem/refactoring/UserRepository.java
@@ -0,0 +1,20 @@
+package com.josdem.refactoring;
+
+public class UserRepository {
+
+	public User findByUuid(String uuid) {
+		return null;
+	}
+
+	public User findByAccount(String account) {
+		return null;
+	}
+
+	public User findByEmail(String email) {
+		return null;
+	}
+
+	public void save(String uuid, String account, String email) {
+	}
+
+}
