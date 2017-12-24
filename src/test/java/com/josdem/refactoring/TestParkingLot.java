6fa534256d96430a497cdfc8f86061bd0f36f105
josdem <joseluis.delacruz@gmail.com>
2017-12-24 11:12:56 -0600

    Setting setup with gradle

--- src/test/java/com/josdem/refactoring/TestParkingLot.java
+++ src/test/java/com/josdem/refactoring/TestParkingLot.java
@@ -1,3 +1,42 @@
 package com.josdem.refactoring;
 
-public class TestParkingLot {}
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestParkingLot {
+	
+	private static final int MAX_CAPACITY = 5;
+
+	private ParkingLot parkingLot = new ParkingLot();
+	
+	@Test
+	public void shouldBeEmpty(){
+		assertTrue(parkingLot.isEmpty());
+	}
+	
+	@Test
+	public void shouldPark(){
+		parkingLot.park();
+		assertFalse(parkingLot.isEmpty());
+	}
+	
+	@Test
+	public void shouldFullAParkingLot(){
+		for(int i=0; i<MAX_CAPACITY; i++){
+			parkingLot.park();
+		}
+		assertTrue(parkingLot.isFull());
+	}
+
+	@Test(expected=RuntimeException.class)
+	public void shouldNotParkSinceIsFull(){
+		for(int i=0; i<MAX_CAPACITY; i++){
+			parkingLot.park();
+		}
+		assertTrue(parkingLot.isFull());
+		parkingLot.park();
+	}
+	
+}
