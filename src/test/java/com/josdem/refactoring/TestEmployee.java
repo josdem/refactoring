77f1aa3b3c772eeafe3064791aec52f96e7fc05e
josdem <joseluis.delacruz@gmail.com>
2017-12-24 15:33:03 -0600

    Deleting unused throws exception

--- src/test/java/com/josdem/refactoring/TestEmployee.java
+++ src/test/java/com/josdem/refactoring/TestEmployee.java
@@ -7,21 +7,21 @@ import org.junit.Test;
 public class TestEmployee {
 	
 	@Test
-	public void shouldGetEngineerSalary() throws Exception {
+	public void shouldGetEngineerSalary() {
 		BigDecimal salary = new BigDecimal(100);
 		Employee engineer = new Engineer();
 		assertEquals(salary, engineer.getPaymentAmount());
 	}
 	
 	@Test
-	public void shouldGetSalesmanSalary() throws Exception {
+	public void shouldGetSalesmanSalary() {
 		BigDecimal salary = new BigDecimal(110);
 		Employee salesman = new Salesman();
 		assertEquals(salary, salesman.getPaymentAmount());
 	}
 	
 	@Test
-	public void shouldGetManagerSalary() throws Exception {
+	public void shouldGetManagerSalary() {
 		BigDecimal salary = new BigDecimal(120);
 		Employee manager = new Manager();
 		assertEquals(salary, manager.getPaymentAmount());
